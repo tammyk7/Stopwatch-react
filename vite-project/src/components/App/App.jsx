818dd2d08ee7a424ff48ef49e8e3dcc9b557d5bd
Tammy <tammy_khan786@hotmail.co.uk>
2022-10-13 12:06:52 +0100

    refactor: refactored all file components

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -1,74 +1,80 @@
-import { useEffect, useState } from 'react'
-import { formatTime } from './utils'
-import './App.css'
-import { Buttons } from '../Buttons/Buttons'
-import Laps from '../Laps/Laps'
+import { useEffect, useState } from "react";
+import { formatTime } from "../utils/utils";
+import "./App.css";
+import { Buttons } from "../Buttons/Buttons";
+import Laps from "../Laps/Laps";
 
 function App() {
-
-const [elapsedTime, setElapsedTime] = useState(0)
-const [isRunning, setIsRunning] = useState(false)
-const [lapData, setLapData] = useState({
-  laps: [],
-  totalLapTime: 0,
-  minLap: Infinity,
-  maxLap: 0
-})
-
-useEffect(() => {
-  if (isRunning) {
-    const startTime = Date.now() - elapsedTime
-    const interval = setInterval(() => setElapsedTime(Date.now() - startTime), 10)
-    return () => clearInterval(interval)
-  }
-}, [isRunning])
-
-const addLap = () => {
-  const currentLapTime = elapsedTime - lapData.totalLapTime
-  setLapData(prevLapData => {
-    return {
-      ...prevLapData,
-      laps: [...prevLapData.laps, currentLapTime],
-      totalLapTime: currentLapTime + lapData.totalLapTime,
-      minLap: currentLapTime < prevLapData.minLap ? currentLapTime : prevLapData.minLap,
-      maxLap: currentLapTime > prevLapData.maxLap ? currentLapTime : prevLapData.maxLap
-    }
+  const [elapsedTime, setElapsedTime] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+  const [lapData, setLapData] = useState({
+    laps: [],
+    totalLapTime: 0,
+    minLap: Infinity,
+    maxLap: 0,
   })
-}
 
-const resetTimer = () => {
-  setIsRunning(false)
-  setElapsedTime(0)
-  setLapData(
-    {
+  useEffect(() => {
+    if (isRunning) {
+      const startTime = Date.now() - elapsedTime;
+      const interval = setInterval(
+        () => setElapsedTime(Date.now() - startTime),
+        10
+      );
+      return () => clearInterval(interval);
+    }
+  }, [isRunning])
+
+  const addLap = () => {
+    const currentLapTime = elapsedTime - lapData.totalLapTime;
+    setLapData((prevLapData) => {
+      return {
+        ...prevLapData,
+        laps: [...prevLapData.laps, currentLapTime],
+        totalLapTime: currentLapTime + lapData.totalLapTime,
+        minLap:
+          currentLapTime < prevLapData.minLap
+            ? currentLapTime
+            : prevLapData.minLap,
+        maxLap:
+          currentLapTime > prevLapData.maxLap
+            ? currentLapTime
+            : prevLapData.maxLap,
+      }
+    })
+  }
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+    setLapData({
       laps: [],
       totalLapTime: 0,
       minLap: Infinity,
-      maxLap: 0
+      maxLap: 0,
     })
-}
+  }
 
-const toggleTimer = () => setIsRunning(!isRunning)
-const lapResetButtonAction = () => !isRunning ? resetTimer() : addLap()
+  const toggleTimer = () => setIsRunning(!isRunning);
+  const lapResetButtonAction = () => (!isRunning ? resetTimer() : addLap())
 
-return (
-  <div>
-    <div className="wrapper">
-      <div className="main">
-        <div className="timer-container">
-          <span>{formatTime(elapsedTime)}</span>
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="main">
+          <div className="timer-container">
+            <span>{formatTime(elapsedTime)}</span>
+          </div>
+          <Buttons
+            isRunning={isRunning}
+            elapsedTime={elapsedTime}
+            toggleTimer={toggleTimer}
+            lapResetButtonAction={lapResetButtonAction}
+          />
+          <Laps lapData={lapData} elapsedTime={elapsedTime} />
         </div>
-      <Buttons 
-        isRunning={isRunning} 
-        elapsedTime={elapsedTime} 
-        toggleTimer={toggleTimer}
-        lapResetButtonAction={lapResetButtonAction}/>
-      <Laps
-        lapData={lapData}
-        elapsedTime={elapsedTime}/>
       </div>
     </div>
-  </div>
-)
+  )
 }
 export default App
--- vite-project/src/components/Buttons/Buttons.jsx
+++ vite-project/src/components/Buttons/Buttons.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 
-export const Buttons = ({isRunning, elapsedTime, toggleTimer, lapResetButtonAction}) => {
+export const Buttons = ({isRunning,elapsedTime,toggleTimer,lapResetButtonAction,}) => {
 
-const startStopButtonText = isRunning ? 'Stop' : 'Start'
-const startStopButtonColor = isRunning ? 'round-button stop-button' : 'round-button start-button'
-const lapResetButtonText = (isRunning || elapsedTime === 0) ? 'Lap' : 'Reset'
-const isLapButtonDisabled = !isRunning && elapsedTime === 0
+  const startStopButtonText = isRunning ? "Stop" : "Start"
+  const startStopButtonColor = isRunning ? "round-button stop-button" : "round-button start-button"
+  const lapResetButtonText = isRunning || elapsedTime === 0 ? "Lap" : "Reset"
+  const isLapButtonDisabled = !isRunning && elapsedTime === 0
 
-return (
+  return (
     <div className="button-container">
-        <button
-            className="round-button lap-button"
-            onClick={lapResetButtonAction}
-            disabled={isLapButtonDisabled}>
-            {lapResetButtonText}
-        </button>
-        <button 
-            className={startStopButtonColor} 
-            onClick={toggleTimer}>
-            {startStopButtonText}
-        </button>
+      <button
+        className="round-button lap-button"
+        onClick={lapResetButtonAction}
+        disabled={isLapButtonDisabled}
+      >
+        {lapResetButtonText}
+      </button>
+      <button className={startStopButtonColor} onClick={toggleTimer}>
+        {startStopButtonText}
+      </button>
     </div>
-);
-};
+  )
+}
